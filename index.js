cf6d87ae4ab22a2839ee7a2cbf8e830355148fc0
Vedant Mahadik <mahadikvedant32@gmail.com>
2024-11-16 12:34:46 +0530

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+import { HermesClient } from "@pythnetwork/hermes-client";
+
+const args = process.argv.slice(2);
+const price_id = [args[0]];
+
+const run = async () => {
+  const connection = new HermesClient("https://hermes.pyth.network", {});
+  // Latest price updates
+  const priceUpdates = await connection.getLatestPriceUpdates(price_id);
+  console.log("##", { ...priceUpdates.parsed[0].ema_price }, "##");
+};
+
+run();
